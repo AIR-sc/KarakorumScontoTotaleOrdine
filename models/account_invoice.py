829bca6e983bca8196c3875344aa1f483e29672b
AIR-sc <73834302+AIR-sc@users.noreply.github.com>
2020-12-18 16:09:03 +0100

    Fixato sconto fattura

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -21,158 +21,78 @@
 #############################################################################
 
 from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.move"
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
 
-    @api.depends(
-        'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual',
-        'line_ids.matched_debit_ids.debit_move_id.move_id.line_ids.amount_residual_currency',
-        'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual',
-        'line_ids.matched_credit_ids.credit_move_id.move_id.line_ids.amount_residual_currency',
-        'line_ids.debit',
-        'line_ids.credit',
-        'line_ids.currency_id',
-        'line_ids.amount_currency',
-        'line_ids.amount_residual',
-        'line_ids.amount_residual_currency',
-        'line_ids.payment_id.state',
-        'line_ids.full_reconcile_id')
-    def _compute_amount(self):
-        for move in self:
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = amount_discount = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.product_uom_qty * line.price_unit
+                amount_tax += line.price_tax
+                amount_discount += (line.product_uom_qty * line.price_unit * line.discount) / 100
+            order.update({
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
+                'amount_discount': amount_discount,
+                'amount_total': amount_untaxed - amount_discount + amount_tax,
+            })
 
-            if move.payment_state == 'invoicing_legacy':
-                # invoicing_legacy state is set via SQL when setting setting field
-                # invoicing_switch_threshold (defined in account_accountant).
-                # The only way of going out of this state is through this setting,
-                # so we don't recompute it here.
-                move.payment_state = move.payment_state
-                continue
+    discount_type = fields.Selection([('percent', 'Percentuale'), ('amount', 'Importo')], string='Tipologia',
+                                     readonly=True,
+                                     states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+                                     default='')
+    discount_rate = fields.Float('Sconto', digits=dp.get_precision('Account'),
+                                 readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
+    amount_untaxed = fields.Monetary(string='Imponibile', store=True, readonly=True, compute='_amount_all',
+                                     track_visibility='always')
+    amount_tax = fields.Monetary(string='Tasse', store=True, readonly=True, compute='_amount_all',
+                                 track_visibility='always')
+    amount_total = fields.Monetary(string='Totale', store=True, readonly=True, compute='_amount_all',
+                                   track_visibility='always')
+    amount_discount = fields.Monetary(string='Sconto', store=True, readonly=True, compute='_amount_all',
+                                      digits=dp.get_precision('Account'), track_visibility='always')
 
-            total_untaxed = 0.0
-            total_untaxed_currency = 0.0
-            total_tax = 0.0
-            total_tax_currency = 0.0
-            total_to_pay = 0.0
-            total_residual = 0.0
-            total_residual_currency = 0.0
-            total = 0.0
-            total_currency = 0.0
-            currencies = set()
-            
-            
-            if move.move_type == 'entry' or move.is_outbound():
-                sign = 1
-            else:
-                sign = -1
-
-            for line in move.line_ids:
-                if line.currency_id:
-                    currencies.add(line.currency_id)
-
-                if move.is_invoice(include_receipts=True):
-                    # === Invoices ===
-
-                    if not line.exclude_from_invoice_tab:
-                        # Untaxed amount.
-                        total_untaxed += sign *(line.quantity * line.price_unit) # line.balance
-                        total_untaxed_currency += sign *(line.quantity * line.price_unit) # line.amount_currency
-                        total += line.balance
-                        total_currency += line.amount_currency
-                    elif line.tax_line_id:
-                        # Tax amount.
-                        total_tax += line.balance
-                        total_tax_currency += line.amount_currency
-                        total += line.balance
-                        total_currency += line.amount_currency
-                    elif line.account_id.user_type_id.type in ('receivable', 'payable'):
-                        # Residual amount.
-                        total_to_pay += line.balance
-                        total_residual += line.amount_residual
-                        total_residual_currency += line.amount_residual_currency
-                else:
-                    # === Miscellaneous journal entry ===
-                    if line.debit:
-                        total += sign *(line.quantity * line.price_unit) # line.balance
-                        total_currency += sign *(line.quantity * line.price_unit) # line.amount_currency
-
-            if move.discount_type == 'percent':
-                move.amount_discount = sum((line.quantity * line.price_unit * line.discount) / 100 for line in move.invoice_line_ids)
-            else:
-                move.amount_discount = move.discount_rate
-            move.amount_untaxed = sign * (total_untaxed_currency if len(currencies) == 1 else total_untaxed)
-            move.amount_tax = sign * (total_tax_currency if len(currencies) == 1 else total_tax)
-            move.amount_total = sign * (total_currency if len(currencies) == 1 else total)
-            move.amount_residual = -sign * (total_residual_currency if len(currencies) == 1 else total_residual)
-            move.amount_untaxed_signed = -total_untaxed
-            move.amount_tax_signed = -total_tax
-            move.amount_total_signed = abs(total) if move.move_type == 'entry' else -total
-            move.amount_residual_signed = total_residual
-
-            currency = len(currencies) == 1 and currencies.pop() or move.company_id.currency_id
-
-            # Compute 'payment_state'.
-            new_pmt_state = 'not_paid' if move.move_type != 'entry' else False
-
-            if move.is_invoice(include_receipts=True) and move.state == 'posted':
-
-                if currency.is_zero(move.amount_residual):
-                    if all(payment.is_matched for payment in move._get_reconciled_payments()):
-                        new_pmt_state = 'paid'
-                    else:
-                        new_pmt_state = move._get_invoice_in_payment_state()
-                elif currency.compare_amounts(total_to_pay, total_residual) != 0:
-                    new_pmt_state = 'partial'
-
-            if new_pmt_state == 'paid' and move.move_type in ('in_invoice', 'out_invoice', 'entry'):
-                reverse_type = move.move_type == 'in_invoice' and 'in_refund' or move.move_type == 'out_invoice' and 'out_refund' or 'entry'
-                reverse_moves = self.env['account.move'].search(
-                    [('reversed_entry_id', '=', move.id), ('state', '=', 'posted'), ('move_type', '=', reverse_type)])
-
-                # We only set 'reversed' state in cas of 1 to 1 full reconciliation with a reverse entry; otherwise, we use the regular 'paid' state
-                reverse_moves_full_recs = reverse_moves.mapped('line_ids.full_reconcile_id')
-                if reverse_moves_full_recs.mapped('reconciled_line_ids.move_id').filtered(lambda x: x not in (
-                        reverse_moves + reverse_moves_full_recs.mapped('exchange_move_id'))) == move:
-                    new_pmt_state = 'reversed'
-
-            move.payment_state = new_pmt_state
-
-    discount_type = fields.Selection([('percent', 'Percentage'), ('amount', 'Amount')], string='Discount Type',
-                                     readonly=True, states={'draft': [('readonly', False)]}, default='percent')
-    discount_rate = fields.Float('Discount Amount', digits=(16, 2), readonly=True,
-                                 states={'draft': [('readonly', False)]})
-    amount_discount = fields.Monetary(string='Discount', store=True, readonly=True, compute='_compute_amount',
-                                      track_visibility='always')
-
-    @api.onchange('discount_type', 'discount_rate', 'invoice_line_ids')
+    @api.onchange('discount_type', 'discount_rate', 'order_line')
     def supply_rate(self):
-        for inv in self:
-            if inv.discount_type == 'percent':
-                for line in inv.line_ids:
-                    line.discount = inv.discount_rate
-                    line._onchange_price_subtotal()
-            else:
-                total = discount = 0.0
-                for line in inv.invoice_line_ids:
-                    total += (line.quantity * line.price_unit)
-                if inv.discount_rate != 0:
-                    discount = (inv.discount_rate / total) * 100
-                else:
-                    discount = inv.discount_rate
-                for line in inv.line_ids:
-                    line.discount = discount
-                    line._onchange_price_subtotal()
 
-            inv._compute_invoice_taxes_by_group()
-    #
+        for order in self:
+            if order.discount_type == 'percent':
+                for line in order.order_line:
+                    line.discount = order.discount_rate
+            elif order.discount_type == 'amount':
+                total = discount = 0.0
+                for line in order.order_line:
+                    total += round((line.product_uom_qty * line.price_unit))
+                if order.discount_rate != 0:
+                    discount = (order.discount_rate / total) * 100
+                else:
+                    discount = order.discount_rate
+                for line in order.order_line:
+                    line.discount = discount
+
+    def _prepare_invoice(self, ):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals.update({
+            'discount_type': self.discount_type,
+            'discount_rate': self.discount_rate,
+        })
+        return invoice_vals
 
     def button_dummy(self):
+
         self.supply_rate()
         return True
 
 
-class AccountInvoiceLine(models.Model):
-    _inherit = "account.move.line"
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
 
     discount = fields.Float(string='Discount (%)', digits=(16, 20), default=0.0)
