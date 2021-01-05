cd2052ce1b28de3ab1917e68f04ebfab87137cf3
AIR-sc <73834302+AIR-sc@users.noreply.github.com>
2021-01-05 18:32:11 +0100

    Lasciati 2 decimali al campo sconto

--- models/sale.py
+++ models/sale.py
@@ -95,4 +95,4 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    discount = fields.Float(string='Discount (%)', digits=(16, 20), default=0.0)
+    discount = fields.Float(string='Discount (%)', digits=(16, 2), default=0.0)
